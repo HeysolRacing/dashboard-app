bd744c741c821e7b61868b0d5f5ed6c3599a1912
LuisZaMe <luis.zamudio.mercado@gmail.com>
2024-03-24 15:38:34 -0600

    control flow statements

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,14 +1,23 @@
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+type Grade = 'A' | 'B' | 'F';
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  template: `<p>control-flow works!</p>`,
+  imports: [CommonModule],
+  templateUrl: './control-flow.component.html',
   styleUrl: './control-flow.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ControlFlowComponent { }
+export default class ControlFlowComponent {
+  public showContent = signal(false);
+  public showGrade = signal<Grade>('A');
+  
+  public showFrameworks = signal(['Angular', 'Vue', 'Svelte', 'Qwik', 'React']);
+  public showFrameworks2 = signal(['Angular']);
+
+
+  public toggleContent() {
+    this.showContent.update((value) => !value);
+  }
+}
