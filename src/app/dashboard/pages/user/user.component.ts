587408ba10fa03e60ff79f51be0441ee606356b7
Heysel <heysel@gmail.com>
2024-04-07 00:06:36 -0600

    adding observable components

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { TitleComponent } from '@shared/title/title.component';
+import { UsersService } from '@services/users.service';
+import { switchMap } from 'rxjs';
+import { User } from '@interfaces/req-response';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TitleComponent],
   templateUrl: './user.component.html',
-  styles: ``
 })
 export default class UserComponent {
+  private route = inject(ActivatedRoute);
+  private usersService = inject(UsersService);
 
+  public user = toSignal(
+     this.route.params.pipe(
+        switchMap(({ id }) => this.usersService.getUserById(id))
+      )
+  )
+
+  public titleLabel = computed(() => {
+    if (this.user()) {
+      return `Welcome :  ${this.user()?.first_name}  ${this.user()?.last_name}.`
+    }
+    return 'Welcome!!'
+  });
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,5 +1,7 @@
-import { Injectable, signal } from '@angular/core';
-import { User } from '@interfaces/req-response';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import type { User, UserResponse, UsersResponse } from '@interfaces/req-response';
+import { delay, map } from 'rxjs';
 
 interface State {
   users: User[];
@@ -9,14 +11,38 @@ interface State {
 @Injectable({
   providedIn: 'root',
 })
-export class UserService {
+
+export class UsersService {
+
+  private http = inject(HttpClient);
+
   #state = signal<State>({
     loading: true,
     users: [],
   });
 
+  public users = computed(() => this.#state().users);
+  public loading = computed(() => this.#state().loading);
+
   constructor() {
-    console.log('Cargando data');
+    this.fetchUser();
   }
-  //https://reqres.in/api/users?page=2
+
+  fetchUser(): void {
+    this.http.get<UsersResponse>('https://reqres.in/api/users')
+      .pipe(delay(1000))
+      .subscribe(res => {
+        this.#state.set({
+          loading: false,
+          users: res.data,
+        })
+      });
+  }
+
+  getUserById(id: string) {
+    return this.http.get<UserResponse>(`https://reqres.in/api/users/${id}`)
+      .pipe(delay(1000),
+        map(resp => resp.data)
+      )
+  }
 }
